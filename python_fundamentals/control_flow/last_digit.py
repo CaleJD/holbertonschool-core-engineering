39ac27220ab9ec140221cb72c46006555867bce3
Cale <14838@holbertonstudents.com>
2026-09-23 17:32:53 +1000

    added last digit

--- python_fundamentals/control_flow/last_digit.py
+++ python_fundamentals/control_flow/last_digit.py
@@ -2,7 +2,7 @@
 
 number = __import__('random').randint(-10000, 10000)
 
-digit = number % 10
+digit = abs(number) % 10
 if digit > 5:
     print(f"Last digit of {number} is {digit} and is greater than 5")
 if digit < 6 and digit != 0:

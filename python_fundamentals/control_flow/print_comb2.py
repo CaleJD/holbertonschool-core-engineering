410ab2d2bb8e1258b5ed1864e901df6e579b9320
Cale <14838@holbertonstudents.com>
2026-09-24 14:42:16 +1000

    fused prints

--- python_fundamentals/control_flow/print_comb2.py
+++ python_fundamentals/control_flow/print_comb2.py
@@ -3,7 +3,4 @@
 for i in range(0, 100):
     print("{:02d}".format(i), end='')
     if i != 99:
-        print(", ", end='')
-    else:
-        print("\n")
-        
+        print("\n" if i == 99 else ", ", end='')
